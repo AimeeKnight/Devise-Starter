9ccc75db42f3f73a9054cfb3b5f80e5ff7da0511
Aimee <AimeeKnight83@gmail.com>
2014-06-14 09:21:30 -0500

    clean up assets

--- config/initializers/carrierwave.rb
+++
@@ -1,8 +0,0 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV['S3_KEY'],                # required
-    :aws_secret_access_key  => ENV['S3_SECRET'],             # required
-  }
-  config.fog_directory  = ENV['S3_BUCKET_NAME']              # required
-end
