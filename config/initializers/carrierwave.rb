ac8443970f800c03b41af9853fd0a15ec66559a4
Aimee <AimeeKnight83@gmail.com>
2014-06-15 16:28:01 -0500

    pull in S3 uploader changes from blogament

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,5 @@
 CarrierWave.configure do |config|
+
   if Rails.env.test?
     config.storage = :file
     config.enable_processing = false
@@ -17,4 +18,5 @@ CarrierWave.configure do |config|
     config.fog_attributes     = {'Cache-Control'=>'max-age=315576000'}
     config.fog_directory      = ENV['S3_BUCKET_NAME']          # required
   end
+
 end
